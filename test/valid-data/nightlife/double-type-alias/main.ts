9082046c7b74d70dd71627b8205fe16cc07579a6
Rob Dempsey <robd@nightlife.com.au>
2020-02-04 14:26:01 +1000

    Fixes issues with nested generics

---
+++ test/valid-data/nightlife/double-type-alias/main.ts
@@ -0,0 +1,24 @@
+
+export interface IStuff {
+    a: string;
+    b: number;
+    c: string;
+}
+
+
+export interface IListsable {
+    lists: Array<IStuff>;
+}
+
+export interface IRequestParams<T> {
+    /**
+     * @ignore
+     */
+    readonly responseType?: T;
+}
+
+export interface IRes {
+    test: IListsable;
+}
+
+export interface MyObject extends Partial<IRequestParams<IRes>> { }
